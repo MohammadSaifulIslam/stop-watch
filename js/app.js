4517ced537f7109f28ffdf1742b8c53a77221e84
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-03-06 14:48:57 +0600

    stopwatch js functional part done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
-document.getElementById('start').addEventListener('click', function() {
-    setInterval(() => {
-        let miliseconds = 0;
-        console.log(++miliseconds)
+let mili = 00;
+let sec = 00;
+let min = 00;
+let hour = 00;
+let timerDisplay = document.getElementById('timer-display');
+let startBtn = document.getElementById('start');
+let pauseBtn = document.getElementById('pause');
+let resetBtn = document.getElementById('reset');
 
-    }, 100);
+let timerId = null;
+
+startBtn.addEventListener('click', function() {
+    if (timerId !== null) {
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
 })
+
+pauseBtn.addEventListener('click', function() {
+    clearInterval(timerId);
+})
+
+resetBtn.addEventListener('click', function() {
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00 : 00`;
+    mili = min = sec = hour = 0;
+})
+
+function startTimer() {
+    mili++;
+    if (mili === 99) {
+        mili = 0;
+        sec++;
+        if (sec === 60) {
+            sec = 0;
+            mili++;
+            if (mili === 60) {
+                hour++;
+            }
+        }
+    }
+
+    let miliString = mili < 10 ? `0${mili}` : mili;
+    let secString = sec < 10 ? `0${sec}` : sec;
+    let minString = min < 10 ? `0${min}` : min;
+    let hourString = hour < 10 ? `0${hour}` : hour;
+
+    timerDisplay.innerHTML = `${hourString} : ${minString} : ${secString} : ${miliString}`
+
+}
